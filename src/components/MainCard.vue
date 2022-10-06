2e3f98941c5c32e6454aa2fbf64dabffc6990e5f
juampiarias <juaarias@itba.edu.ar>
2022-10-06 10:57:22 -0300

    componentes de mainpage agregados

---
+++ src/components/MainCard.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <v-row>
+      <v-col>
+        <h1 class="text1">
+          Crea tu proxima rutina de la manera mas simple posible,<p></p> comenza clickeando en “Crear rutina”
+        </h1>
+      </v-col>
+      <v-col>
+        <v-img class="img1"
+               max-height="400"
+               max-width="450"
+               src="https://www.lanacion.com.ar/resizer/LmQ0IdGvJZGgjCmY7F4wDL8zdNg=/1200x800/smart/filters:format(webp):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/UEDSYPTFZRANLI62Z7U4PXTLNE.jpg"
+        ></v-img>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-img class="img2"
+               max-height="400"
+               max-width="450"
+               src="https://www.musculaciontotal.com/wp-content/uploads/2014/09/banco-plano-con-manos-juntas.jpg.webp"
+        ></v-img>
+      </v-col>
+      <v-col>
+        <h1 class="text2">
+          En “Ejercicios” puedes encontrar todos los ejercicios que<p></p>  necesitas para armar tu rutina
+        </h1>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainCard"
+}
+</script>
+
+<style scoped>
+.text1{
+  margin-left: 130px;
+  margin-top: 150px;
+  font-weight: bold;
+  font-size: x-large;
+}
+.img1{
+  margin-left: 250px;
+  margin-top: 50px;
+}
+.text2{
+  margin-left: 80px;
+  margin-top: 100px;
+  font-weight: bold;
+  font-size: x-large;
+}
+.img2{
+  margin-left: 100px;
+}
+</style>
---
+++ src/components/SubBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div >
+    <v-container >
+    <v-row
+        justify="center"
+    >
+      <v-col md="auto">
+        <v-btn class="button"
+            color="blue "
+            x-large="true"
+        >
+          Ejercicios
+        </v-btn>
+      </v-col>
+      <v-col md="auto">
+        <v-btn class="button"
+            color="blue"
+            x-large="true"
+        >
+          Crear rutina
+        </v-btn>
+      </v-col>
+
+      <v-col md="auto">
+          <v-btn class="button"
+                 x-large="true"
+                 color="blue"
+            >
+            Mis rutinas
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SubBar"
+}
+</script>
+
+<style scoped>
+.button{
+  width: 300px;
+  font-size: 25px;
+  font-weight: bold;
+  color: white;
+}
+</style>
